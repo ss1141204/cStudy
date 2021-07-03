7e9bbd6667efc15f1243f7797ef4d899edb77cfb
ss1141204 <ss1141204@gmail.com>
2021-07-03 14:18:51 +0900

    [add] 프로그래머스_해시_완주하지 못한 선수

---
+++ Programmers/Hash1.java
@@ -0,0 +1,36 @@
+//프로그래머스 - 해시 - 완주하지 못한 선수
+import java.util.*;
+
+public class Hash1 {
+	
+	public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Map<String, Integer> player = new HashMap<>();	//선수 map
+        
+        for(String p : participant) {
+        	player.put(p, player.getOrDefault(p, 0) + 1);	//<선수명, 0 혹은 value++>
+        }
+        
+        for(String c : completion) {
+        	player.put(c, player.getOrDefault(c, 0) - 1);	//<선수명, 0 혹은 value-->
+        }
+        
+        for(String p : player.keySet()) {
+        	if(player.get(p) != 0) {
+        		return p;
+        	}
+        }
+        
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		String[] participant = {"leo", "kiki","eden", "leo"};
+		String[] completion = {"eden", "kiki", "leo"};
+		
+		System.out.println(solution(participant, completion));
+		
+	}
+
+}
