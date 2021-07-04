2008541ef84cc3b67c300bd05b70a34e0d3a6278
name <name@DESKTOP-PEFV71I>
2021-07-04 18:20:51 +0900

    [add] JAVA_Date Class 주요 메소드 활용

---
+++ PrJava/codingStudy/PrDateClass.java
@@ -0,0 +1,21 @@
+package codingStudy;
+
+import java.util.Date;
+
+public class PrDateClass {
+
+	public static void main(String[] args) {
+		
+		@SuppressWarnings("deprecation")
+		Date date1 = new Date("Mon, 04 July 2020 11:52:03 +0900");
+		Date date2 = new Date("Mon, 04 July 2021 12:09:17 +0900");
+		
+		System.out.println(date1.toString());
+		System.out.println(date1.getTime());
+		System.out.println(date1.after(date2));
+		System.out.println(date1.before(date2));
+		
+		date1.setTime(7000000);
+	}
+
+}
